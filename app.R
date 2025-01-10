b36d03c9a6ff42085b79f18726b168413e7c1c25
mdotson2001 <mikayla_dotson@brown.edu>
2025-01-10 17:29:29 -0500

    1/10/25 App

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+
 library(readr)
 library(shiny)
 library(shinyjs)
@@ -6,7 +7,7 @@ library(dplyr)
 library(sf)
 library(shinyWidgets)
 library(DT)
-library(markdown) # Ensure this is included to render Markdown
+library(markdown) 
 
 # Read sitedata from CSV file
 sitedata <- readr::read_csv("www/Summary Illegal Dumping Site Inventory (1).csv")
@@ -15,70 +16,120 @@ sitedata <- readr::read_csv("www/Summary Illegal Dumping Site Inventory (1).csv"
 coordinates <- st_as_sf(sitedata, coords = c("Longitude", "Latitude"), crs = 4326) %>%
   st_transform(crs = 4326)
 
-print(coordinates)
 
 # Define UI
 ui <- navbarPage(
     "Illegal Dumping Sites in Mississippi", id="nav",
-        includeCSS("style.css"),
-                 tabPanel("Map",icon = icon("map"),
-                          div(
-                            class="outer",
-                              actionButton("button", "User Guide"),
-                              leafletOutput("map", width = "100%", height = "700px"),
-                              absolutePanel(
-                                id = "controls", class = "panel panel-default", fixed = TRUE,
-                                draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                width = 330, height = "auto",
-                                style = "background-color: rgba(255, 255, 255, 0.8); padding: 10px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);",
-                                h2("Search Options"),
-                                pickerInput(inputId = "source", label = "Select Zip Code", choices = unique(sitedata$Zip_Code),
-                                            selected = NULL,
-                                            multiple = TRUE),
-                                pickerInput(inputId = "City_source", label = "Select City",
-                                            choices = unique(sitedata$City),
-                                            selected = NULL, 
-                                            multiple = TRUE)
     
-                                
-                              ),
-                              verbatimTextOutput("site_info")
-                          )
-                 ),
+  includeCSS("www/style.css"),
+  tags$head(
+    tags$script(HTML("
+    $(document).on('click', '.go-to-marker', function() {
+      var site_id = $(this).attr('id').replace('marker-', '');
+      Shiny.setInputValue('go_to_marker', site_id);
+    });
+  "))
+  ),
+  tags$head(
+    tags$script(HTML("
+    $(document).on('click', '.go-to-table', function() {
+      var site_id = $(this).attr('id').replace('go-to-table-', '');  // Extract Site_ID
+      Shiny.setInputValue('popup_more_info', site_id);  // Send Site_ID to Shiny
+    });
+  "))
+  ),
+  tags$head(
+    tags$style(HTML("
+    .modal {
+      display: block !important;
+      visibility: visible !important;
+    }
+  "))
+  ),
+  
+  tabPanel(
+          "Map",
+          icon = icon("map"),
+          div(
+              class="outer",
+              actionButton("button", "User Guide", class = "action-button"),
+              leafletOutput("map", width = "100%", height = "700px"),
+              absolutePanel(
+                    id = "controls", 
+                    class = "panel panel-default", 
+                    fixed = TRUE,
+                    draggable = TRUE,
+                    top = 60, left = "auto", right = 20, bottom = "auto",
+                    width = 330, height = "auto",
+                    h2("Search Options"),
+                    pickerInput(
+                        inputId = "source", 
+                        label = "Select Zip Code", 
+                        choices = unique(sitedata$Zip_Code),
+                        selected = NULL,
+                        multiple = TRUE),
+                    pickerInput(
+                        inputId = "City_source", 
+                        label = "Select City",
+                        choices = unique(sitedata$City),
+                        selected = NULL, 
+                        multiple = TRUE
+                        )
+                ),
+            verbatimTextOutput("site_info")
+        )
+    ),
                  
-                 tabPanel("Data Table", icon = icon("table"),
-                          DT::dataTableOutput("data_table")
-                          ),
-                 
-                 tabPanel("Data Dictionary", icon = icon("book"),
-                          htmlOutput("data_dictionary")
-                 ),
-                 navbarMenu("About", icon = icon("info-circle"),
-                            tabPanel("About the Developer", htmlOutput("about_developer")),
-                            tabPanel("Mission", 
-                                     column(6,
-                                            br(),
-                                            h2("Why Have You Developed An Illegal Dumping Site Reporting Tool?"),
-                                            h5(p("Community Noise Lab believes that information is a crucial first-step in eliminating the illegal dumping issue in Mississippi. By providing all with information about illegal dumping sites in your community and what is in them, you have the data you need to demand change from your elected officials.")
-                                            )
-                                     )
+      tabPanel(
+        "Data Table",
+        icon = icon("table"),
+        DT::dataTableOutput("data_table")
+      ),
+  
+      tabPanel(
+        "Data Dictionary",
+        icon = icon("book"),
+        htmlOutput("data_dictionary")
+      ),
+  
+      navbarMenu(
+          "About",
+          icon = icon("info-circle"),
+          tabPanel(
+              "About the Developer", 
+              htmlOutput("about_developer")
+          ),
+          tabPanel(
+              "Mission",
+              column(
+                6,
+                br(),
+                h2("Why Have You Developed An Illegal Dumping Site Reporting Tool?"),
+                h5(
+                  p(
+                    "Community Noise Lab believes that information is a crucial first-step in eliminating the illegal dumping issue in Mississippi. By providing all with information about illegal dumping sites in your community and what is in them, you have the data you need to demand change from your elected officials.")
+                    )
+                  )
                                      
-                            ),
-                            tabPanel("Reporting a Site", fluid = TRUE,
-                                     
-                                     column(6,
-                                            br(),
-                                            h2("How to Report an Illegal Dumping Site"),
-                                            h5(p("Our team, which consists of local high school and community college students, will go out and assess the illegal dumping site for a wide variety of environmental concerns.If you would like to report an illegal dumping site in your community, you can fill out our",
-                                                 a("Illegal Dumping Site Reporting Tool.",
-                                                   href="https://www.jotform.com/form/241495365663162"))
-                                            )
-                                            
-                                            
-                                     ))
-                            
-                 )
-)                 
+          ),
+            tabPanel(
+              "Reporting a Site", 
+                  column(
+                    6,
+                    br(),
+                    h2("How to Report an Illegal Dumping Site"),
+                    h5(
+                      p(
+                        "Our team, which consists of local high school and community college students, will go out and assess the illegal dumping site for a wide variety of environmental concerns.If you would like to report an illegal dumping site in your community, you can fill out our",
+                        a("Illegal Dumping Site Reporting Tool.",href="https://www.jotform.com/form/241495365663162")
+                        )
+                      )
+                      )
+              )
+      )
+)             
+
+
 # Define server logic
 server <- function(input, output, session) {
   
@@ -93,12 +144,16 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet(coordinates) %>%
       addProviderTiles(provider = "Esri.WorldTopoMap") %>%
-      addCircleMarkers(data = sitedata,
-                       lng = ~Longitude,
-                       lat = ~Latitude,
-                       popup = ~Site_ID,
-                       layerId = ~Site_ID, 
-                       radius = 4, color = "00732", fillOpacity = 0.7)
+      addCircleMarkers(
+        data = sitedata,
+        lng = ~Longitude,
+        lat = ~Latitude,
+        popup = ~Site_ID,
+        layerId = ~Site_ID, 
+        radius = 4, 
+        color = "#00732", 
+        fillOpacity = 0.7
+        )
   })
 
   
@@ -107,43 +162,113 @@ server <- function(input, output, session) {
     click <- input$map_marker_click
     site_data <- sitedata %>% filter(Site_ID == click$id)
     
-    if (nrow(site_data) > 0) {
-      tryCatch({
-        leafletProxy("map") %>%
-          clearPopups() %>%
-          addPopups(
-            lng = site_data$Longitude,
-            lat = site_data$Latitude,
-            popup = paste(
-              "<div style='width: 300px; max-height: 400px; overflow-y: auto; font-family: Arial, sans-serif;'>",
-              "<h3 style='margin-top: 0;'>", "Site ", site_data$Site_ID, "</h3>",
-              "<table style='width: 100%; border-collapse: collapse;'>",
-              "<tr><th style='text-align:left; padding: 5px; border-bottom: 1px solid #ddd;'>Perimeter</th>",
-              "<td style='padding: 5px; border-bottom: 1px solid #ddd;'>", site_data$Perimeter_m, "</td></tr>",
-              "<tr><th style='text-align:left; padding: 5px; border-bottom: 1px solid #ddd;'>City</th>",
-              "<td style='padding: 5px; border-bottom: 1px solid #ddd;'>", site_data$City, "</td></tr>",
-              "<tr><th style='text-align:left; padding: 5px; border-bottom: 1px solid #ddd;'>Longitude</th>",
-              "<td style='padding: 5px; border-bottom: 1px solid #ddd;'>", site_data$Longitude, "</td></tr>",
-              "<tr><th style='text-align:left; padding: 5px; border-bottom: 1px solid #ddd;'>Latitude</th>",
-              "<td style='padding: 5px; border-bottom: 1px solid #ddd;'>", site_data$Latitude, "</td></tr>",
-              "<tr><th style='text-align:left; padding: 5px; border-bottom: 1px solid #ddd;'>Zip Code</th>",
-              "<td style='padding: 5px; border-bottom: 1px solid #ddd;'>", site_data$Zip_Code, "</td></tr>",
-              if (!is.na(site_data$Image_link) && site_data$Image_link != "") {
-                paste("<tr><th colspan='2' style='padding: 5px; border-bottom: 1px solid #ddd;'><img src='", site_data$Image_link, "' width='100%' height='auto'></th></tr>")
-              } else {
-                "<tr><th colspan='2' style='padding: 5px; border-bottom: 1px solid #ddd;'>No image available.</th></tr>"
-              },
-              "<tr><th colspan='2' style='padding: 5px; border-bottom: 1px solid #ddd; text-align:center;'>",
-              "<tr><th style='text-align:left; padding: 5px; border-bottom: 1px solid #ddd;'>Date Last Assessed </th>",
-              "<td style='padding: 5px; border-bottom: 1px solid #ddd;'>", site_data$Updates, "</td></tr>",
-              "</table>",
-              "</div>"
-            )
+    # Handle case where site_data is empty
+    if (nrow(site_data) == 0) {
+      # Display a default popup for unmatched markers
+      leafletProxy("map") %>%
+        clearPopups() %>%
+        addPopups(
+          lng = click$lng,
+          lat = click$lat,
+          popup = "No data available for this site."
+        )
+      return()
+    }
+    
+    # Proceed with the normal popup logic
+    leafletProxy("map") %>%
+      clearPopups() %>%
+      addPopups(
+        lng = ifelse(!is.na(site_data$Longitude), site_data$Longitude, 0),
+        lat = ifelse(!is.na(site_data$Latitude), site_data$Latitude, 0),
+        popup = paste0(
+          "<div class='scrollable-popup'>",
+          "<div class= 'popup-title'> Site Id: ", ifelse(!is.na(site_data$Site_ID), site_data$Site_ID, "Unknown"), "</div><br>", 
+          "<strong> Perimeter:</strong> ", ifelse(!is.na(site_data$Perimeter_m), site_data$Perimeter_m, "N/A"), "<br>", 
+          "<strong> City:</strong> ", ifelse(!is.na(site_data$City), site_data$City, "Unknown"), "<br>", 
+          "<strong> Coordinates:</strong> ", ifelse(!is.na(site_data$Longitude), site_data$Longitude, "N/A"), ", ", 
+          ifelse(!is.na(site_data$Latitude), site_data$Latitude, "N/A"), "<br>",
+          "<strong> Zip Code:</strong> ", ifelse(!is.na(site_data$Zip_Code), site_data$Zip_Code, "N/A"), "<br>",
+          if (!is.na(site_data$Image_link) && site_data$Image_link != "") {
+            paste0("<strong>Site Image:</strong><br><img src='", site_data$Image_link, "' width='200px' height='auto'><br>")
+          } else {
+            "<strong>Site Image:</strong> No image available.<br>"
+          },
+          "<strong>Key Contaminants:</strong> ", ifelse(!is.na(site_data$Key_Cont), sitedata$Key_Cont, "Not Available"), "<br>", 
+          "<strong>VOC 10 Levels:</strong> ", ifelse(!is.na(site_data$Voc_level), sitedata$Voc_level, "Not Available"), "<br>",
+          "<strong>PM 10 Levels:</strong> ", ifelse(!is.na(site_data$PM_10), sitedata$PM_10, "Not Available"), "<br>", 
+          "<strong>PM 2/5 Levels:</strong> ", ifelse(!is.na(site_data$PM_2.5), sitedata$PM_2.5, "Not Available"), "<br>",
+          "<strong>Air quality:</strong> ", ifelse(!is.na(site_data$Air), sitedata$Air, "Not Available"), "<br>", 
+          "<strong>Average Noise Level:</strong> ", ifelse(!is.na(site_data$Noise), sitedata$Noise, "Not Available"), "<br>", 
+          "<strong>Soil Sample Collected?:</strong> ", ifelse(!is.na(site_data$Soil_sample), sitedata$Soil_sample, "No"), "<br>", 
+          "<strong> Updates:</strong> ", ifelse(!is.na(site_data$Updates), site_data$Updates, "No updates"), "<br>",
+          "<button id='go-to-table-", site_data$Site_ID, "' class='btn btn-primary go-to-table'>More Info</button>",
+          "</div>"
+        )
+      )
+  })  
+  
+  observeEvent(input$popup_more_info, {
+    req(input$popup_more_info)
+    
+    updateTabsetPanel(session, inputId = "nav", selected = "Data Table")
+  })
+
+  
+# Data Table ####
+output$data_table <- renderDataTable({
+    datatable(
+      sitedata %>%
+        mutate(
+          Action = paste0(
+            '<button class="btn btn-primary go-to-marker" id="marker-', Site_ID, '">Go to Marker</button>'
           )
-      })
+        ),
+      escape = FALSE,  # Allow HTML in the table
+      options = list(pageLength = 10, scrollX = TRUE)
+    )
+  })
+## Goes to Marker from Data Table#
+  observeEvent(input$go_to_marker, {
+    site_id <- input$go_to_marker  # Get the clicked Site_ID
+    site_data <- sitedata %>% filter(Site_ID == site_id)
+    
+    if (nrow(site_data) > 0) {
+      updateTabsetPanel(session, inputId = "nav", selected = "Map")
+      leafletProxy("map") %>%
+        setView(lng = site_data$Longitude, lat = site_data$Latitude, zoom = 15) %>%
+        clearPopups() %>%
+        addPopups(
+          lng = site_data$Longitude,
+          lat = site_data$Latitude,
+          popup = paste0(
+            "<div class='scrollable-popup'>",
+            "<strong> Site Id:</strong> ", ifelse(!is.na(site_data$Site_ID), site_data$Site_ID, "Unknown"), "<br>", 
+            "<strong> Perimeter:</strong> ", ifelse(!is.na(site_data$Perimeter_m), site_data$Perimeter_m, "N/A"), "<br>", 
+            "<strong> City:</strong> ", ifelse(!is.na(site_data$City), site_data$City, "Unknown"), "<br>", 
+            "<strong> Coordinates:</strong> ", ifelse(!is.na(site_data$Longitude), site_data$Longitude, "N/A"), ", ", 
+            ifelse(!is.na(site_data$Latitude), site_data$Latitude, "N/A"), "<br>",
+            "<strong> Zip Code:</strong> ", ifelse(!is.na(site_data$Zip_Code), site_data$Zip_Code, "N/A"), "<br>",
+            if (!is.na(site_data$Image_link) && site_data$Image_link != "") {
+              paste0("<strong>Site Image:</strong><br><img src='", site_data$Image_link, "' width='200px' height='auto'><br>")
+            } else {
+              "<strong>Site Image:</strong> No image available.<br>"
+            },
+            "<strong>Key Contaminants:</strong> ", ifelse(!is.na(site_data$Key_Cont), sitedata$Key_Cont, "Not Available"), "<br>", 
+            "<strong>VOC 10 Levels:</strong> ", ifelse(!is.na(site_data$Voc_level), sitedata$Voc_level, "Not Available"), "<br>",
+            "<strong>PM 10 Levels:</strong> ", ifelse(!is.na(site_data$PM_10), sitedata$PM_10, "Not Available"), "<br>", 
+            "<strong>PM 2/5 Levels:</strong> ", ifelse(!is.na(site_data$PM_2.5), sitedata$PM_2.5, "Not Available"), "<br>",
+            "<strong>Air quality:</strong> ", ifelse(!is.na(site_data$Air), sitedata$Air, "Not Available"), "<br>", 
+            "<strong>Average Noise Level:</strong> ", ifelse(!is.na(site_data$Noise), sitedata$Noise, "Not Available"), "<br>", 
+            "<strong>Soil Sample Collected?:</strong> ", ifelse(!is.na(site_data$Soil_sample), sitedata$Soil_sample, "No"), "<br>", 
+            "<strong> Updates:</strong> ", ifelse(!is.na(site_data$Updates), site_data$Updates, "No updates"), "<br>",
+            "</div>"
+          )
+        )
     }
   })
   
+# Filters data# 
 observe({
   filtered <- reactive_data()
   leafletProxy("map") %>%
@@ -156,9 +281,9 @@ observe({
 })
 
   # Render the Data Table and necessary actions for button in DT###################################
-  output$data_table <-renderDataTable({
-    datatable(sitedata, options = list(pageLength = 10, scrollX = TRUE))
-                                     })
+ # output$data_table <-renderDataTable({
+    #datatable(sitedata, options = list(pageLength = 10, scrollX = TRUE))
+                                    # })
 
   # Render the about.Rmd file#################
   output$about_developer <- renderUI({ includeMarkdown("About_the_developer.Rmd") })
@@ -173,8 +298,8 @@ observe({
           If you are interested in knowing if there are any reported illegal dumping sites in your community, you can search our database by zip code and city.
 For each reported illegal dumping site in our database, you will find a wealth of information, including photos, soil pollution reports, 
           water pollution reports, air quality levels, and noise pollution levels.",
-      easyClose = TRUE, # Allows closing the modal by clicking outside
-      footer = modalButton("Close") # Adds a close button
+      easyClose = TRUE, 
+      footer = modalButton("Close") 
     ))
   })
 }
